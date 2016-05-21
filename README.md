76f2357f32ee0ee297590e1837e301e946948175
ianrahman <ian.alexander.rahman@gmail.com>
2016-05-21 19:00:44 -0400

    Added empty project and standard Learn License and Contributing files

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Goals
 Dictionaries are a fundamental data type, and it's important to be comfortable using them. In this lab you're going to practice creating, writing to, accessing, and mutating dictionaries. You'll also get additional experience working with nested dictionaries.
 
-## Instructions
-### 1. 
+## Review
+Let's go over what a Dictionary is.
---
+++ objc-dictionary-practice/AppDelegate.m
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.m
+//  objc-dictionary-practice
+//
+//  Created by Flatiron School on 5/21/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    /**
+     
+     *  Write your code here!
+     
+     */
+    
+    // Do not alter
+    return YES;  //
+}   ///////////////
+
+@end
